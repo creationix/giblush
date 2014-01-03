e11333649f923a4f35ff57a28b3ad82c6fc259a2
Tim Caswell <tim@creationix.com>
2014-01-02 22:32:09 -0600

    Add sample data

---
+++ test/main.js
@@ -0,0 +1,9 @@
+var domBuilder = require('./dombuilder.js');
+
+// Clear the body
+document.body.textContent = "";
+// Add a nice header and paragraph
+document.body.appendChild(domBuilder([
+  ["h1", "Hello World"],
+  ["p", "This is a sample webpage served from js-git dynamically"]
+]));
