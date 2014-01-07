49f8a2265a4fda7ec20df2751f2453288d5a341c
Tim Caswell <tim@creationix.com>
2014-01-07 16:38:47 -0600

    Update cjs to use smart caching too

--- cjs-filter.js
+++ cjs-filter.js
@@ -9,134 +9,151 @@ function cjs(req, callback) {
 
   // console.log("CJS", req);
   var etag = 'W/"' + sha1(req.root + ":" + req.target.hash) + '"';
-  return callback(null, {etag: etag, fetch: function (callback) {
-    console.log("FETCH ME!");
-  }});
-  parallel(req.args.map(function (file) {
-    return req.repo.servePath(req.root, pathJoin(req.base, file));
-  }), function (err, entries) {
-    if (err) return callback(err);
-    console.log("ENTRIES", entries);
-  });
-
   var modules = {};  // compiled modules
   var packagePaths = {}; // key is base + name , value is full path
   var aliases = {}; // path aliases from the "browser" directive in package.json
   var path = '-';
-  processJs(path, input, function (err) {
-    if (err) return callback(err);
-    var out;
-    try { out = gen({
-      initial: path,
-      modules: modules
-    }, true) + "\n"; }
-    catch (err) { return callback(err); }
-    callback(null, out);
-  });
 
-  function processJs(path, js, callback) {
-    var deps = mine(js);
-    modules[path] = { type: "javascript", value: js, deps: deps };
-    next(0);
-    function next(index) {
-      var dep = deps[index];
-      if (!dep) return callback(null, path);
-      resolveModule(pathJoin(path, '..'), dep.name, function (err, newPath) {
+  return callback(null, {etag: etag, fetch: fetch});
+
+  function fetch(callback) {
+    req.target.fetch(onInput);
+
+    function onInput(err, input) {
+      if (err) return callback(err);
+      if (Buffer.isBuffer(input)) input = input.toString();
+      processJs(path, input, function (err) {
         if (err) return callback(err);
-        dep.newPath = newPath;
-        next(index + 1);
+        var out;
+        try { out = gen({
+          initial: path,
+          modules: modules
+        }, true) + "\n"; }
+        catch (err) { return callback(err); }
+        callback(null, out);
       });
     }
-  }
 
-  function resolveModule(base, path, callback) {
-    if (path[0] === ".") {
-      return resolvePath(pathJoin(base, path), callback);
-    }
-
-    // non-local requires are assumed to belong to packages
-    var index = path.indexOf("/");
-    var name = index < 0 ? path : path.substr(0, index);
-    return loadPackage(base, name, onPackage);
-
-    function onPackage(err, metaPath) {
-      if (metaPath === undefined) return callback(err);
-      if (index < 0) path = metaPath;
-      else path = pathJoin(metaPath, path.substr(index));
-      return resolvePath(path, callback);
-    }
-  }
-
-  function resolvePath(path, callback) {
-    if (path in aliases) path = aliases[path];
-    if (path in modules) return callback(null, path);
-    if (/\.js$/.test(path)) {
-      return loader(path, false, onJavaScript);
-    }
-    if (/\.json$/.test(path)) {
-      return loader(path, false, onJson);
-    }
-    if (/#txt$/.test(path)) {
-      return loader(path.substr(0, path.length - 4), false, onText);
-    }
-    if (/#bin$/.test(path)) {
-      return loader(path.substr(0, path.length - 4), true, onBinary);
-    }
-    return callback(new Error("Invalid path extension: " + path));
-
-    function onJavaScript(err, js) {
-      if (err) return callback(err);
-      processJs(path, js, callback);
-    }
-
-    function onJson(err, json) {
-      if (json === undefined) return callback(err);
-      var value;
-      try { value = JSON.parse(json); }
-      catch (err) { return callback(err); }
-      modules[path] = { type: "json", value: value };
-      callback(null, path);
-    }
-
-    function onText(err, text) {
-      if (text === undefined) return callback(err);
-      modules[path] = { type: "text", value: text };
-      callback(null, path);
-    }
-
-    function onBinary(err, binary) {
-      if (binary === undefined) return callback(err);
-      modules[path] = { type: "binary", value: binary };
-      callback(null, path);
-    }
-
-  }
-
-  function loadPackage(base, name, callback) {
-    var key = pathJoin(base, name);
-    if (key in packagePaths) return callback(null, packagePaths[key]);
-    var metaPath = pathJoin(base, "node_modules", name, "package.json");
-    loader(metaPath, false, function (err, json) {
-      if (err) return callback(err);
-      if (!json) {
-        if (base === "/" || base === ".") return callback();
-        return loadPackage(pathJoin(base, ".."), name, callback);
+    function processJs(path, js, callback) {
+      var deps = mine(js);
+      modules[path] = { type: "javascript", value: js, deps: deps };
+      next(0);
+      function next(index) {
+        var dep = deps[index];
+        if (!dep) return callback(null, path);
+        resolveModule(pathJoin(path, '..'), dep.name, function (err, newPath) {
+          if (err) return callback(err);
+          dep.newPath = newPath;
+          next(index + 1);
+        });
       }
-      var meta;
-      try { meta = JSON.parse(json); }
-      catch (err) { return callback(err); }
-      base = pathJoin(metaPath, "..");
-      packagePaths[key] = base;
-      if (meta.main) {
-        aliases[base] = pathJoin(base, meta.main);
+    }
+
+    function resolveModule(base, path, callback) {
+      if (path[0] === ".") {
+        return resolvePath(pathJoin(base, path), callback);
       }
-      if (meta.browser) {
-        for (var original in meta.browser) {
-          aliases[pathJoin(base, original)] = pathJoin(base, meta.browser[original]);
+
+      // non-local requires are assumed to belong to packages
+      var index = path.indexOf("/");
+      var name = index < 0 ? path : path.substr(0, index);
+      return loadPackage(base, name, onPackage);
+
+      function onPackage(err, metaPath) {
+        if (metaPath === undefined) return callback(err);
+        if (index < 0) path = metaPath;
+        else path = pathJoin(metaPath, path.substr(index));
+        return resolvePath(path, callback);
+      }
+    }
+
+    function resolvePath(path, callback) {
+      if (path in aliases) path = aliases[path];
+      if (path in modules) return callback(null, path);
+      if (/\.js$/.test(path)) {
+        return loader(path, false, onJavaScript);
+      }
+      if (/\.json$/.test(path)) {
+        return loader(path, false, onJson);
+      }
+      if (/#txt$/.test(path)) {
+        return loader(path.substr(0, path.length - 4), false, onText);
+      }
+      if (/#bin$/.test(path)) {
+        return loader(path.substr(0, path.length - 4), true, onBinary);
+      }
+      return callback(new Error("Invalid path extension: " + path));
+
+      function onJavaScript(err, js) {
+        if (err) return callback(err);
+        processJs(path, js, callback);
+      }
+
+      function onJson(err, json) {
+        if (json === undefined) return callback(err);
+        var value;
+        try { value = JSON.parse(json); }
+        catch (err) { return callback(err); }
+        modules[path] = { type: "json", value: value };
+        callback(null, path);
+      }
+
+      function onText(err, text) {
+        if (text === undefined) return callback(err);
+        modules[path] = { type: "text", value: text };
+        callback(null, path);
+      }
+
+      function onBinary(err, binary) {
+        if (binary === undefined) return callback(err);
+        modules[path] = { type: "binary", value: binary };
+        callback(null, path);
+      }
+
+    }
+
+    function loader(path, binary, callback) {
+      req.repo.servePath(req.root, path, null, function (err, res) {
+        if (!res) return callback(err);
+        res.fetch(function (err, data) {
+          if (err) return callback(err);
+          if (Buffer.isBuffer(data)) {
+            if (!binary) data = data.toString();
+          }
+          else {
+            if (binary) data = new Buffer(data);
+          }
+          callback(null, data);
+        });
+      });
+    }
+
+    function loadPackage(base, name, callback) {
+      var key = pathJoin(base, name);
+      if (key in packagePaths) return callback(null, packagePaths[key]);
+      var metaPath = pathJoin(base, "node_modules", name, "package.json");
+      loader(metaPath, false, function (err, json) {
+        if (err) return callback(err);
+        if (!json) {
+          if (base === "/" || base === ".") return callback();
+          return loadPackage(pathJoin(base, ".."), name, callback);
         }
-      }
-      callback(null, base);
-    });
+        var meta;
+        try { meta = JSON.parse(json); }
+        catch (err) { return callback(err); }
+        base = pathJoin(metaPath, "..");
+        packagePaths[key] = base;
+        if (meta.main) {
+          aliases[base] = pathJoin(base, meta.main);
+        }
+        if (meta.browser) {
+          for (var original in meta.browser) {
+            aliases[pathJoin(base, original)] = pathJoin(base, meta.browser[original]);
+          }
+        }
+        callback(null, base);
+      });
+    }
   }
 
 }
