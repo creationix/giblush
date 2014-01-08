2cf772c73df7c9bfc2d85afd14268337fdc73b40
Tim Caswell <tim@creationix.com>
2014-01-08 16:16:50 -0600

    Allow appcache list to be external

--- appcache-filter.js
+++ appcache-filter.js
@@ -3,8 +3,34 @@ var sha1 = require('js-git/lib/sha1.js');
 module.exports = appcache;
 var pathJoin = require('js-linker/pathjoin.js');
 var parallel = require('js-git/lib/parallel.js');
+var mime = "text/cache-manifest";
 
 function appcache(req, callback) {
+  // If the file list is external, we use the root and file hash for etag
+  if (req.target) {
+    var etag = 'W/"' + sha1(req.root + "-" + req.target.hash) + '"';
+    return callback(null, {etag: etag, mime: mime, fetch: function (callback) {
+      req.target.fetch(function (err, input) {
+        if (err) return callback(err);
+        input = ("" + input).split("\n").filter(Boolean);
+        req.args = req.args.concat(input);
+        render(req, callback);
+      });
+    }});
+  }
+
+  // If it's all args, we can generate the actual manifest without any I/O
+  // and get better caching semantics.
+  render(req, function (err, manifest) {
+    if (err) return callback(err);
+    var etag = 'W/"' + sha1(manifest) + '"';
+    callback(null, {etag: etag, mime: mime, fetch:function (callback) {
+      callback(null, manifest);
+    }});
+  });
+}
+
+function render(req, callback) {
   parallel(req.args.map(function (file) {
     return req.repo.servePath(req.root, pathJoin(req.base, file));
   }), function (err, entries) {
@@ -13,9 +39,6 @@ function appcache(req, callback) {
     entries.forEach(function(entry, i) {
       manifest += req.args[i] + " # " + entry.etag + "\n";
     });
-    var etag = '"' + sha1(manifest) + '"';
-    callback(null, {etag:etag,fetch:function (callback) {
-      callback(null, manifest);
-    }});
+    callback(null, manifest);
   });
 }
